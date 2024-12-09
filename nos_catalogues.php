5d1a246a532b602f32ffe682600dc109399a3415
EvrardMayembo <evrard.mayembo@gmail.com>
2024-12-09 19:09:39 +0100

    Add files via upload

---
+++ nos_catalogues.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html><!DOCTYPE html>
+<html>
+<head>
+	<title>ABA</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body style="background:url(ABA_BACKGROUND.jpg); background-repeat: no-repeat; background-size: 100vw 100vh; background-attachment: fixed; ">
+
+	<nav class="navbar">
+		<ul>
+			<li><a href="index.php">ACCUEIL</a></li>
+			<li><a href="produits_services.php">PRODUITS & SERVICES</a></li>
+			<li><a href="nos_catalogues.php">NOS CATLOGUES</a></li>
+			<li><a href="ingenierie_recherche.php">INGENIERIE & RECHERCHE</a></li>
+			<li><a href="contact.php">CONTACT</a></li>
+		</ul>
+	</nav>
+	<header>
+		HEADER GOES HERE
+	</header>
+	<div class="main">
+		Main
+	</div>
+	<footer>
+		Footer
+	</footer>
+	
+</body>
+</html>
